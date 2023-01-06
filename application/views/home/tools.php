b0633de66d8e94a656b0b30531bacd5024bd6c7f
aris <arisegisaputra@gmail.com>
2023-01-06 18:26:59 +0700

    themplate layout

---
+++ application/views/home/tools.php
@@ -0,0 +1,134 @@
+<div class="row g-7 w-xxl-850px">
+  <!--begin::Col-->
+  <div class="col-xxl-5">
+    <!--begin::Card-->
+    <div class="card border-0 shadow-none h-lg-100" style="background-color: #243763">
+      <!--begin::Card body-->
+      <div class="card-body d-flex flex-column flex-center pb-0 pt-15">
+        <!--begin::Wrapper-->
+        <div class="px-10 mb-10">
+          <!--begin::Heading-->
+          <h3 class="text-white mb-2 fw-boldest ttext-center text-uppercase mb-6">Ngode Tools</h3>
+          <!--end::Heading-->
+          <!--begin::List-->
+          <div class="mb-7">
+            <!--begin::Item-->
+            <div class="d-flex align-items-center mb-2">
+              <!--begin::Svg Icon | path: icons/duotune/arrows/arr001.svg-->
+              <span class="svg-icon svg-icon-4 svg-icon-white opacity-75 me-3">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                  <path d="M14.4 11H3C2.4 11 2 11.4 2 12C2 12.6 2.4 13 3 13H14.4V11Z" fill="black" />
+                  <path opacity="0.3" d="M14.4 20V4L21.7 11.3C22.1 11.7 22.1 12.3 21.7 12.7L14.4 20Z" fill="black" />
+                </svg>
+              </span>
+              <!--end::Svg Icon-->
+              <span class="text-white opacity-75">Average Stock market</span>
+            </div>
+            <!--end::Item-->
+            <!--begin::Item-->
+            <div class="d-flex align-items-center mb-2">
+              <!--begin::Svg Icon | path: icons/duotune/arrows/arr001.svg-->
+              <span class="svg-icon svg-icon-4 svg-icon-white opacity-75 me-3">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                  <path d="M14.4 11H3C2.4 11 2 11.4 2 12C2 12.6 2.4 13 3 13H14.4V11Z" fill="black" />
+                  <path opacity="0.3" d="M14.4 20V4L21.7 11.3C22.1 11.7 22.1 12.3 21.7 12.7L14.4 20Z" fill="black" />
+                </svg>
+              </span>
+              <!--end::Svg Icon-->
+              <span class="text-white opacity-75">Md5 Password Generator</span>
+            </div>
+            <!--end::Item-->
+            <!--begin::Item-->
+            <div class="d-flex align-items-center mb-2">
+              <!--begin::Svg Icon | path: icons/duotune/arrows/arr001.svg-->
+              <span class="svg-icon svg-icon-4 svg-icon-white opacity-75 me-3">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                  <path d="M14.4 11H3C2.4 11 2 11.4 2 12C2 12.6 2.4 13 3 13H14.4V11Z" fill="black" />
+                  <path opacity="0.3" d="M14.4 20V4L21.7 11.3C22.1 11.7 22.1 12.3 21.7 12.7L14.4 20Z" fill="black" />
+                </svg>
+              </span>
+              <!--end::Svg Icon-->
+              <span class="text-white opacity-75">Image To base64</span>
+            </div>
+            <!--end::Item-->
+          </div>
+          <!--end::List-->
+          <!--begin::Link-->
+          <a href="https://ngodeid.com/" class="btn btn-hover-rise text-white bg-white bg-opacity-10 text-uppercase fs-7 fw-bolder">Go To Ngodeid</a>
+          <!--end::Link-->
+        </div>
+        <!--end::Wrapper-->
+        <!--begin::Illustrations-->
+        <img class="mw-100 h-225px mx-auto mb-lg-n18" src="<?=base_url('/')?>theme/assets/media/misc/1.png" />
+        <!--end::Illustrations-->
+      </div>
+      <!--end::Card body-->
+    </div>
+    <!--end::Card-->
+  </div>
+  <!--end::Col-->
+  <!--begin::Col-->
+  <div class="col-xxl-7">
+    <!--begin::Row-->
+    <div class="row g-lg-7">
+      <!--begin::Col-->
+      <div class="col-sm-6">
+        <!--begin::Card-->
+        <a href="javascript:;"  class="card border-0 shadow-none min-h-200px mb-7" style="background-color: #FF6E31">
+          <!--begin::Card body-->
+          <div class="card-body d-flex flex-column flex-center text-center">
+            <!--begin::Illustrations-->
+            <img class="mw-100 h-100px mb-7 mx-auto" src="<?=base_url('/')?>theme/assets/media/misc/stock_market.png" />
+            <!--end::Illustrations-->
+            <!--begin::Heading-->
+            <h4 class="text-white fw-bolder text-uppercase">Average Stock market</h4>
+            <!--end::Heading-->
+          </div>
+          <!--end::Card body-->
+        </a>
+        <!--end::Card-->
+      </div>
+      <!--end::Col-->
+      <!--begin::Col-->
+      <div class="col-sm-6">
+        <!--begin::Card-->
+        <a href="javascript:;" class="card border-0 shadow-none min-h-200px mb-7" style="background-color: #AD8E70">
+          <!--begin::Card body-->
+          <div class="card-body d-flex flex-column flex-center text-center">
+            <!--begin::Illustrations-->
+            <img class="mw-100 h-100px mb-7 mx-auto" src="<?=base_url('/')?>theme/assets/media/misc/md5.png" />
+            <!--end::Illustrations-->
+            <!--begin::Heading-->
+            <h4 class="text-white fw-bolder text-uppercase">Md5 Password Generator</h4>
+            <!--end::Heading-->
+          </div>
+          <!--end::Card body-->
+        </a>
+        <!--end::Card-->
+      </div>
+      <!--end::Col-->
+    </div>
+    <!--end::Row-->
+    <div class="row g-lg-7">
+      <!--begin::Col-->
+      <div class="col-sm-6">
+        <!--begin::Card-->
+        <a href="javascript:;"  class="card border-0 shadow-none min-h-200px mb-7" style="background-color: #749F82">
+          <!--begin::Card body-->
+          <div class="card-body d-flex flex-column flex-center text-center">
+            <!--begin::Illustrations-->
+            <img class="mw-100 h-100px mb-7 mx-auto" src="<?=base_url('/')?>theme/assets/media/misc/base64.png" />
+            <!--end::Illustrations-->
+            <!--begin::Heading-->
+            <h4 class="text-white fw-bolder text-uppercase">Image To base64</h4>
+            <!--end::Heading-->
+          </div>
+          <!--end::Card body-->
+        </a>
+        <!--end::Card-->
+      </div>
+      <!--end::Col-->
+    </div>
+  </div>
+  <!--end::Col-->
+</div>
---
+++ application/views/layout/main.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+
+<html lang="en">
+	<!--begin::Head-->
+	<head><base href="">
+		<title>Tools Ngodeid - Tools untuk mempermudah kita dalam permasalahan yang terjadi di sekitar kita.</title>
+		<meta charset="utf-8" />
+		<meta name="description" content="Tools untuk mempermudah kita dalam permasalahan yang terjadi di sekitar kita." />
+		<meta name="keywords" content="Tools Average Stock market, Md5 Password Generator, Image To base64." />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta property="og:locale" content="en_US" />
+		<meta property="og:type" content="article" />
+		<meta property="og:title" content="Tools untuk mempermudah kita dalam permasalahan yang terjadi di sekitar kita. Tools Average Stock market, Md5 Password Generator, Image To base64." />
+		<meta property="og:url" content="<?=base_url('tools')?>" />
+		<meta property="og:site_name" content="Tools Ngodeid - Tools untuk mempermudah kita dalam permasalahan yang terjadi di sekitar kita." />
+		<link rel="canonical" href="<?=base_url()?>" />
+		<link rel="shortcut icon" href="<?=base_url('/')?>theme/assets/media/misc/icon.png" />
+		<!--begin::Fonts-->
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+		<!--end::Fonts-->
+		<!--begin::Page Vendor Stylesheets(used by this page)-->
+		<link href="<?=base_url('/')?>theme/assets/plugins/custom/fullcalendar/fullcalendar.bundle.css" rel="stylesheet" type="text/css" />
+		<!--end::Page Vendor Stylesheets-->
+		<!--begin::Global Stylesheets Bundle(used by all pages)-->
+		<link href="<?=base_url('/')?>theme/assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
+		<link href="<?=base_url('/')?>theme/assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
+		<!--end::Global Stylesheets Bundle-->
+	</head>
+	<!--end::Head-->
+	<!--begin::Body-->
+	<body id="kt_body" class="page-bg">
+		<!--begin::Main-->
+		<!--begin::Root-->
+		<div class="d-flex flex-column flex-root">
+			<!--begin::Page-->
+			<div class="page launcher sidebar-enabled d-flex flex-row flex-column-fluid me-lg-5" id="kt_page">
+				<!--begin::Content-->
+				<div class="d-flex flex-row-fluid">
+					<!--begin::Container-->
+					<div class="d-flex flex-column flex-row-fluid align-items-center">
+						<!--begin::Menu-->
+						<div class="d-flex flex-column flex-column-fluid mb-5 mb-lg-10">
+							<!--begin::Brand-->
+							<div class="d-flex flex-center pt-10 pt-lg-0 mb-10 mb-lg-0 h-lg-225px">
+								<!--begin::Sidebar toggle-->
+								<div class="btn btn-icon btn-active-color-primary w-30px h-30px d-lg-none me-4 ms-n15" id="kt_sidebar_toggle">
+									<!--begin::Svg Icon | path: icons/duotune/abstract/abs015.svg-->
+									<span class="svg-icon svg-icon-1">
+										<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+											<path d="M21 7H3C2.4 7 2 6.6 2 6V4C2 3.4 2.4 3 3 3H21C21.6 3 22 3.4 22 4V6C22 6.6 21.6 7 21 7Z" fill="black" />
+											<path opacity="0.3" d="M21 14H3C2.4 14 2 13.6 2 13V11C2 10.4 2.4 10 3 10H21C21.6 10 22 10.4 22 11V13C22 13.6 21.6 14 21 14ZM22 20V18C22 17.4 21.6 17 21 17H3C2.4 17 2 17.4 2 18V20C2 20.6 2.4 21 3 21H21C21.6 21 22 20.6 22 20Z" fill="black" />
+										</svg>
+									</span>
+									<!--end::Svg Icon-->
+								</div>
+								<!--end::Sidebar toggle-->
+								<!--begin::Logo-->
+								<a href="<?=base_url('')?>">
+									<img alt="Logo" src="<?=base_url('/')?>theme/assets/media/misc/logongode.png" class="h-75px h-lg-80px" />
+								</a>
+								<!--end::Logo-->
+							</div>
+							<!--end::Brand-->
+							<!--begin::Row-->
+							<?=$content?>
+							<!--end::Row-->
+						</div>
+						<!--end::Menu-->
+						<!--begin::Footer-->
+						<div class="d-flex flex-column-auto flex-center">
+							<!--begin::Navs-->
+							<ul class="menu fw-bold order-1">
+								<li class="menu-item">
+									<a href="https://ngodeid.com/" target="_blank" class="menu-link text-white opacity-50 opacity-100-hover px-3">Home</a>
+								</li>
+								<li class="menu-item">
+									<a href="https://ngodeid.com/about/" target="_blank" class="menu-link text-white opacity-50 opacity-100-hover px-3">About</a>
+								</li>
+								<li class="menu-item">
+									<a href="https://ngodeid.com/privacy-policy/" target="_blank" class="menu-link text-white opacity-50 opacity-100-hover px-3">Privacy policy</a>
+								</li>
+							</ul>
+							<!--end::Navs-->
+						</div>
+						<!--end::Footer-->
+					</div>
+					<!--begin::Content-->
+				</div>
+				<!--begin::Content-->
+				<!--begin::Sidebar-->
+        <div class="d-none d-sm-block">
+				<div id="kt_sidebar" class="sidebar px-5 py-5 py-lg-8 px-lg-11 " data-kt-drawer="true" data-kt-drawer-name="sidebar" data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="375px" data-kt-drawer-direction="end" data-kt-drawer-toggle="#kt_sidebar_toggle">
+				
+					<div class="mb-5 mb-lg-8 " id="kt_sidebar_body">
+            <img width="100%" src="https://www.1980media.com/wp-content/uploads/2021/03/google-adsense-icon3-1920-1170x700.jpg">
+					</div>
+				
+				</div>
+        </div>
+				<!--end::Sidebar-->
+			</div>
+			<!--end::Page-->
+		</div>
+		<!--end::Root-->
+		<!--end::Main-->
+	
+		<!--begin::Javascript-->
+		<script>var hostUrl = "<?=base_url('/')?>theme/assets/";</script>
+		<!--begin::Global Javascript Bundle(used by all pages)-->
+		<script src="<?=base_url('/')?>theme/assets/plugins/global/plugins.bundle.js"></script>
+		<script src="<?=base_url('/')?>theme/assets/js/scripts.bundle.js"></script>
+		<!--end::Global Javascript Bundle-->
+		<!--begin::Page Vendors Javascript(used by this page)-->
+		<script src="<?=base_url('/')?>theme/assets/plugins/custom/fullcalendar/fullcalendar.bundle.js"></script>
+		<!--end::Page Vendors Javascript-->
+		<!--begin::Page Custom Javascript(used by this page)-->
+		<script src="<?=base_url('/')?>theme/assets/js/custom/utilities/modals/create-project/type.js"></script>
+		<script src="<?=base_url('/')?>theme/assets/js/custom/utilities/modals/create-project/budget.js"></script>
+		<script src="<?=base_url('/')?>theme/assets/js/custom/utilities/modals/create-project/settings.js"></script>
+		<script src="<?=base_url('/')?>theme/assets/js/custom/utilities/modals/create-project/team.js"></script>
+		<script src="<?=base_url('/')?>theme/assets/js/custom/utilities/modals/create-project/targets.js"></script>
+		<script src="<?=base_url('/')?>theme/assets/js/custom/utilities/modals/create-project/files.js"></script>
+		<script src="<?=base_url('/')?>theme/assets/js/custom/utilities/modals/create-project/complete.js"></script>
+		<script src="<?=base_url('/')?>theme/assets/js/custom/utilities/modals/create-project/main.js"></script>
+		<!-- <script src="<?=base_url('/')?>theme/assets/js/custom/utilities/modals/create-account.js"></script> -->
+		<!--end::Page Custom Javascript-->
+		<!--end::Javascript-->
+	</body>
+	<!--end::Body-->
+</html>
